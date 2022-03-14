4811d17a7dc940812ca1152c36b22a13feee27e4
kuldeepsagar <kuldeep.sagar@gmail.com>
2022-03-14 16:25:16 +0530

    changed provider config

--- iacs/ex_tr_aws_basic/provider.tf
+++ iacs/ex_tr_aws_basic/provider.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.45.0"
+      #version = "~> 3.45.0"
     }
   }
 
-  required_version = ">= 0.13.0"
+  #required_version = ">= 0.13.0"
 }
 
 provider "aws" {
